3c48a1915634408f250d94facf991b15a83ea859
zjr <15010910013@163.com>
2018-04-28 21:24:03 +0800

    第二次提交

--- src/com/jinru/com/TestGit.java
+++ src/com/jinru/com/TestGit.java
@@ -3,8 +3,9 @@ package com.jinru.com;
 public class TestGit {
 
 	public static void main(String[] args) {
-		System.out.println("总有刁明想害朕！");
-
+		System.out.println("总有刁民想害朕！");
+		System.out.println("总有不爱朕的女人");
+		
 	}
 
 }
